c8e6245aa5db85a501d4ed84c2626c8496505657
Edwin Moreno <edwinmoreno77@gmail.com>
2022-08-23 20:25:19 -0400

    habilidades fue creado

---
+++ index.js
@@ -0,0 +1,12 @@
+
+//Boton de Menu del navbar
+
+const navToggle = document.querySelector('.nav-toggle');
+
+const navMenu = document.querySelector('.nav-menu');
+
+navToggle.addEventListener('click', () => {
+
+    navMenu.classList.toggle('nav-menu_visible');
+
+});
